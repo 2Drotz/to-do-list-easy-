5f94527d2178e3eb842109c1bba95db4d90bfd9c
Dmitry <mandrin2013@mail.ru>
2022-08-06 13:30:09 +0300

    commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#input'),
+   allList = document.querySelector('.allList'),
+   eraser = document.querySelector('#eraser');
+
+
+
+eraser.addEventListener('click', () => {
+   allList.textContent = '';
+   input.value = '';
+});
+
+input.addEventListener('keydown', (e) => {
+   if (e.key == 'Enter') {
+      addList();
+   }
+});
+
+function addList() {
+   let h2 = document.createElement('h2');
+   h2.textContent = '- ' + input.value;
+   input.value = '';
+
+   allList.insertAdjacentElement('beforeend', h2);
+
+   h2.addEventListener('click', () => {
+      h2.style.textDecoration = 'line-through';
+   });
+}
+
+
